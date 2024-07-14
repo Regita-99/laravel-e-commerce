933ba7f7eaeeb12fdc1f1d6f00b971388447083f
regita-99 <regitarose9@gmail.com>
2024-07-14 19:53:54 +0700

    detail info

--- app/Http/Controllers/DetailProdukController.php
+++ app/Http/Controllers/DetailProdukController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 
 class DetailProdukController extends Controller
 {
-    public function index(){
+    public function index($id){
         return view('Front.detailProduk');
     }
     public function show($id){
